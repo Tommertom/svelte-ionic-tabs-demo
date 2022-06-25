6255d3cdd279661537ec72a81bad9c93fdc2c080
Tommertom <Tom.Gruintjes@gmail.com>
2022-06-26 00:54:52 +0200

    first commit

---
+++ src/routes/_module.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { defineCustomElements } from "@ionic/pwa-elements/loader";
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    // defineCustomElements(window);
+  });
+</script>
+
+<ion-app>
+  <div class="ion-page" id="main">
+    <slot />
+  </div>
+</ion-app>

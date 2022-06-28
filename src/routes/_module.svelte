8f469304b8ec721b618894f4256a35622a1189c3
Tommertom <Tom.Gruintjes@gmail.com>
2022-06-28 22:43:54 +0200

    Small fix

--- src/routes/_module.svelte
+++ src/routes/_module.svelte
@@ -1,10 +1,4 @@
 <script lang="ts">
-  import { defineCustomElements } from "@ionic/pwa-elements/loader";
-  import { onMount } from "svelte";
-
-  onMount(() => {
-    // defineCustomElements(window);
-  });
 </script>
 
 <ion-app>
